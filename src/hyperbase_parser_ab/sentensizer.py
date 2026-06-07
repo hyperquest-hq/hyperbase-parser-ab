1f153e4772aea79bb3b91dddc46a8ac987dca611
Telmo Menezes <telmo@telmomenezes.net>
2026-06-07 12:39:36 +0200

    switching to sat-3l-sm sentensizer

--- src/hyperbase_parser_ab/sentensizer.py
+++ src/hyperbase_parser_ab/sentensizer.py
@@ -3,7 +3,7 @@ from wtpsplit import SaT
 
 class Sentensizer:
     def __init__(self) -> None:
-        self.sat: SaT = SaT("sat-3l")
+        self.sat: SaT = SaT("sat-3l-sm")
 
     def sentensize(self, text: str) -> list[str]:
         return [str(sentence) for sentence in (self.sat.split(text))]
